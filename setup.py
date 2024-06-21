3eefe105f98af505c5b72afe29e869a7bf8c7c9e
Sasaank <sasaank.aalla@gmail.com>
2024-06-21 14:01:52 +0530

    change

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='IndoNav',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Sasaank',
+    author_email='',
+    description=''
+)
