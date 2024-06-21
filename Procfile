ef0cbc46f87b40971532e046ce88e11d5a2604a3
Sasaank <sasaank.aalla@gmail.com>
2024-06-21 13:18:04 +0530

    Deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python webpage_flask.py
--- webpage_flask.py
+++ webpage_flask.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import heapq
+import os
 
 app = Flask(__name__)
 
@@ -342,4 +343,5 @@ def get_path():
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=5000, debug=True)
